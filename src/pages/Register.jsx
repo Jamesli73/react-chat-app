ad3299b63e861e5db9d0cccea3af47fd8fb6d266
James Li <j2668li@uwaterloo.ca>
2022-11-19 23:46:59 -0500

    progress on scss

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,14 +4,17 @@ export const Register = () => {
   return (
     <div className='formContainer'>
         <div className='formWrapper'>
-          <span className='logo'>Lama Chat</span>
+          <span className='logo'>Chat App</span>
           <span className='title'>Register</span>
             <form>
-                <input type="text" placeholder='display name'/>
+                <input type="text" placeholder='username'/>
                 <input type="email" placeholder='email'/>
                 <input type="password" placeholder='password'/>
-                <input type="file" />
-                <button>Sign up</button>
+                <input style={{display:"none"}}type="file" id='file'/>
+                <label htmlFor="file">
+                  <img src="" alt="" />
+                </label>
+                <button>Sign</button>
             </form>
             <p>Do you have an account? Login</p>
         </div>
